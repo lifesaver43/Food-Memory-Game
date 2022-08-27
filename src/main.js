4f05933b162e92d8a7d51eef9f33d05af5264605
lifesaver43 <innocentnwafor17@gmail.com>
2022-08-27 05:05:52 +0100

    yet to be styled

---
+++ src/main.js
@@ -0,0 +1,133 @@
+document.addEventListener("DOMContentLoaded", () => {
+      //card options
+      const cardArray = [
+            {
+                  name: "fries",
+                  img: "/images/fries.png"
+            },
+            {
+              name: 'cheeseburger',
+              img: '/images/cheeseburger.png'
+            },
+            {
+              name: 'ice-cream',
+              img: '/images/ice-cream.png'
+            },
+            {
+              name: 'pizza',
+              img: '/images/pizza.png'
+            },
+            {
+              name: 'milkshake',
+              img: '/images/milkshake.png'
+            },
+            {
+              name: 'hotdog',
+              img: '/images/hotdog.png'
+            },
+            {
+              name: 'fries',
+              img: '/images/fries.png'
+            },
+            {
+              name: 'cheeseburger',
+              img: '/images/cheeseburger.png'
+            },
+            {
+              name: 'ice-cream',
+              img: '/images/ice-cream.png'
+            },
+            {
+              name: 'pizza',
+              img: '/images/pizza.png'
+            },
+            {
+              name: 'milkshake',
+              img: '/images/milkshake.png'
+            },
+            {
+              name: 'hotdog',
+              img: '/images/hotdog.png'
+            }
+      ]
+
+      cardArray.sort(() => 0.5 - Math.random())
+      console.log(cardArray)
+
+      const grid = document.querySelector(".grid");
+      const resultDisplay = document.querySelector("#result");
+
+      let cardsChosen = []
+      let cardsChosenIds = []
+      let cardsWon = []
+
+      function createBoard() {
+        for(i = 0; i < cardArray.length; i++) {
+          const card = document.createElement("img")
+          card.setAttribute("src", "/images/blank.png")
+          card.setAttribute("data-id", i)
+          card.addEventListener("click", flipCard)
+          grid.appendChild(card)
+        }
+      }
+
+
+      function flipCard() {
+        let cardId = this.getAttribute("data-id")
+        cardsChosen.push(cardArray[cardId].name)
+        cardsChosenIds.push(cardId)
+        this.setAttribute("src", cardArray[cardId].img)
+        if(cardsChosen.length === 2) {
+          setTimeout(checkForMatch, 500)
+        }
+        console.log(cardsChosenIds)
+      }
+
+      function checkForMatch() {
+        const cards = document.querySelectorAll("img")
+        const optionOneId = cardsChosenIds[0];
+        const optionTwoId = cardsChosenIds[1];
+
+        if(optionOneId == optionTwoId) {
+          alert("You have clicked the same image!")
+          cards[optionOneId].setAttribute("src", "/images/blank.png")
+          cards[optionTwoId].setAttribute("src", "/images/blank.png")
+        } else if (cardsChosen[0] === cardsChosen[1]) {
+          alert("You have found a match!")
+          cards[optionOneId].setAttribute("src", "/images/white.png")
+          cards[optionTwoId].setAttribute("src", "/images/white.png")
+          cards[optionOneId].removeEventListener("click", flipCard)
+          cards[optionTwoId].removeEventListener("click", flipCard)
+          cardsWon.push(cardsChosen)
+        } else {
+          cards[optionOneId].setAttribute("src", "/images/blank.png")
+          cards[optionTwoId].setAttribute("src", "/images/blank.png")
+          alert("Sorry, try again")
+        }
+
+        cardsChosen = []
+        cardsChosenIds = []
+        resultDisplay.innerHTML = cardsWon.length
+        if (cardsWon.length === 6) {
+          resultDisplay.innerHTML = "Congratulations, You won!"
+        }
+
+        console.log(cardsChosen)
+        console.log(cardsWon)
+      }
+
+      createBoard()
+
+
+
+
+
+
+
+
+// stopped at 10:26
+      // let cardsPot = Math.random() * cards.length
+      // let cardShop = Math.floor(cardsPot)
+      // const cardsNum = cards[cardShop]
+
+});
